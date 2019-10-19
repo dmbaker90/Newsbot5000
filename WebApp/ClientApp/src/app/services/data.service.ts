3b24d0015d54509f9315a6a161b57038de29fbc1
David Baker <dbaker@gmail.com>
2019-10-18 19:36:28 -0700

    angular scaffolding

---
+++ WebApp/ClientApp/src/app/services/data.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class DataService {
+    baseUrl = environment.apiUrl;
+
+    constructor(private http: HttpClient) { }
+
+    public getValues(): Observable<string[]> {
+        const url = `${this.baseUrl}/Values`;
+        return this.http.get<string[]>(url);
+    }
+}
